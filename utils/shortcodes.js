6c3b01481726149b2dde212d15bdec80b5338291
Greg Ives <gregives774@gmail.com>
2020-08-04 11:39:08 +0100

    Add picture shortcode for responsive images

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,6 +1,7 @@
 const addStylesheetShortcodes = require('./shortcodes/stylesheet')
 const addJavascriptShortcodes = require('./shortcodes/javascript')
 const assetShortcode = require('./shortcodes/asset')
+const pictureShortcode = require('./shortcodes/picture')
 
 module.exports = function(config, options) {
   addStylesheetShortcodes(config, options)
@@ -8,6 +9,9 @@ module.exports = function(config, options) {
 
   // Bind options to the first argument of asset shortcode function
   config.addShortcode('asset', assetShortcode.bind(null, options))
+
+  // Bind options to the first argument of picture shortcode function
+  config.addShortcode('picture', pictureShortcode.bind(null, options))
 
   // Add shortcodes
 }
---
+++ utils/shortcodes/picture.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const path = require('path')
+const sharp = require('sharp')
+
+// Asset shortcode for saving hashed assets
+const saveAsset = require('./asset')
+
+// Sizes for responsive image
+const SIZES = [320, 640, 1280, 1920]
+
+async function saveImageFormat(options, image, format) {
+  // Format image and reduce quality
+  const formatted = image.clone().toFormat(format)[format]({
+    quality: 75
+  })
+
+  // Save buffer of formatted image
+  const buffer = await formatted.toBuffer()
+  return saveAsset(options, buffer, format)
+}
+
+// Get the average color from an image
+async function getAverageColor(image) {
+  // Resize to one pixel and get raw buffer
+  const buffer = await image.clone().resize(1).raw().toBuffer()
+  // Convert values to percentages
+  const values = [...buffer].map((value) => `${(value * 100 / 255).toFixed(0)}%`)
+  // Output rgb or rgba color
+  return `${values.length < 4 ? 'rgb' : 'rgba'}(${values.join(',')})`
+}
+
+module.exports = async function(options, src, alt, sizes = '100vw', loading = 'lazy') {
+  if (alt === undefined)
+    throw new Error('Images should always have an alt tag')
+
+  // TODO: Where should the source path resolve from?
+  const imagePath = path.join(process.cwd(), src)
+
+  // Original image in sharp
+  const original = sharp(imagePath)
+
+  // Get metadata from original image
+  const { format, height, width } = await original.metadata()
+
+  // Average color used for background while image loads
+  const color = await getAverageColor(original)
+
+  // Resize image for responsive-ness
+  const images = SIZES.map((width) => {
+    return original.clone().resize(width)
+  })
+
+  // Save responsive images in same format
+  const sameFormat = await Promise.all(images.map(async (image, index) => {
+    const filename = await saveImageFormat(options, image, format)
+    return `${filename} ${SIZES[index]}w`
+  }))
+
+  // Save responsive images in webp format
+  const webpFormat = await Promise.all(images.map(async (image, index) => {
+    const filename = await saveImageFormat(options, image, 'webp')
+    return `${filename} ${SIZES[index]}w`
+  }))
+
+  // Fallback image
+  const fallback = sameFormat[sameFormat.length - 1].split(' ')[0]
+
+  // Aspect ratio for padding-bottom
+  const ratio = (height / width * 100).toFixed(3)
+
+  return `
+    <picture style="background-color:${color};padding-bottom:${ratio}%">
+      <source srcset="${webpFormat.join(',')}" sizes="${sizes}" type="image/webp">
+      <source srcset="${sameFormat.join(',')}" sizes="${sizes}" type="image/${format}">
+      <img src="${fallback}" alt="${alt}" loading="${loading}">
+    </picture>
+  `
+}
