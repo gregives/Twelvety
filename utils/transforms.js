f728ef3c1e960a77eb650277249dc84df92f2806
Greg Ives <gregives774@gmail.com>
2020-08-07 11:24:29 +0100

    Move critical transform into folder

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,36 +1,10 @@
-const critical = require('critical')
-const { JSDOM } = require('jsdom')
-
-// Twelvety options
-const twelvety = require('@12ty')
+const critical = require('./transforms/critical')
 
 // Require local minify functions
 const minify = require('./minify')
 
 module.exports = function(config) {
-  config.addTransform('critical', async function(content, outputPath) {
-    if (outputPath.endsWith('.html') && twelvety.env === 'production') {
-      const { css } = await critical.generate({
-        base: 'dist/',
-        html: content,
-        minify: false,
-        width: 2560,
-        height: 1600
-      })
-
-      // Append critical styles using JSDOM
-      const dom = new JSDOM(content)
-      const { document } = dom.window
-      const style = document.createElement('style')
-      style.innerHTML = css
-      document.head.appendChild(style)
-
-      // Return serialized JSDOM
-      return dom.serialize()
-    }
-
-    return content
-  })
+  config.addTransform('critical', critical)
 
   config.addTransform('format', function(content, outputPath) {
     if (outputPath.endsWith('.html')) {
---
+++ utils/transforms/critical.js
@@ -0,0 +1,29 @@
+const { JSDOM } = require('jsdom')
+const critical = require('critical')
+
+// Twelvety options
+const twelvety = require('@12ty')
+
+module.exports = async function(content, outputPath) {
+  if (outputPath.endsWith('.html') && twelvety.env === 'production') {
+    const { css } = await critical.generate({
+      base: 'dist/',
+      html: content,
+      minify: false,
+      width: 2560,
+      height: 1600
+    })
+
+    // Append critical styles to head using JSDOM
+    const dom = new JSDOM(content)
+    const { document } = dom.window
+    const style = document.createElement('style')
+    style.innerHTML = css
+    document.head.appendChild(style)
+
+    // Return serialized JSDOM
+    return dom.serialize()
+  }
+
+  return content
+}
