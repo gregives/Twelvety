724174d78ab62a7d2f4027f766b14a7dd0486f23
Greg Ives <gregives774@gmail.com>
2020-08-07 20:40:40 +0100

    Extract critical CSS after append shortcode

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,12 +1,12 @@
-const critical = require('./transforms/critical')
 const append = require('./transforms/append')
+const critical = require('./transforms/critical')
 
 // Require local minify functions
 const minify = require('./minify')
 
 module.exports = function(config) {
-  config.addTransform('critical', critical)
   config.addTransform('append', append)
+  config.addTransform('critical', critical)
 
   config.addTransform('format', function(content, outputPath) {
     if (outputPath.endsWith('.html')) {
